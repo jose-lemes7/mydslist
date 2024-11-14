e4b52262b1b206eeabd42871517c6559fe8a3af9
Jose Lemes <jose.lemes7@gmail.com>
2024-11-14 09:05:23 -0300

    first commit

---
+++ src/main/java/com/devsuperior/mydslist/MydslistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mydslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/mydslist/MydslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mydslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
